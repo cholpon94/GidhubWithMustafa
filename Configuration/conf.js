ca796b51f47d9b2dae55801f1bd3c31585bbbeaa
Cholpon Erkinova <cholponerkinova94@gmail.com>
2018-10-28 19:41:11 -0400

    This is my first offical trial commit

---
+++ Configuration/conf.js
@@ -0,0 +1,45 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var HtmlReporter = require('protractor-beautiful-reporter');
+exports.config = {
+   
+    directConnect : true,
+  
+   capabilities: {
+    browserName: 'firefox',
+    chromeOptions: {
+        args: [
+            '--start-fullscreen','--incognito'
+        ]
+    }
+  },
+  
+  specs: ['../Tests/HomePage.spec.js'], 
+  //suites:['../Tests/BankManagerSimple.spec.js/'],
+  
+onPrepare: function () {
+    //browser.driver.manage().window().maximize();
+    jasmine.getEnv().addReporter(new SpecReporter({
+        displayFailuresSummary: true,
+        displayFailuredSpec: true,
+        displaySuiteNumber: true,
+        displaySpecDuration: true,
+        showstack: false
+      }));
+      // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
+      jasmine.getEnv().addReporter(new HtmlReporter({
+        baseDirectory: '../report/screenshots',
+        preserveDirectory: false,
+        screenshotsSubfolder: 'images',
+         jsonsSubfolder: 'jsons',
+         docName: 'Caribbean-Report.html'
+     }).getJasmine2Reporter());
+  
+},
+    
+    jasmineNodeOpts: {
+        showColors: true, 
+        defaultTimeoutInterval: 100000,    
+        print: function() {}
+        
+}
+}
